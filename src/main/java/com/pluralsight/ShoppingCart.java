33ceb4717c843595a7903296309605c740efebd0
Anne Marie King <amking23@gmail.com>
2018-10-29 13:37:41 -0500

    Update ShoppingCart.java

--- src/main/java/com/pluralsight/ShoppingCart.java
+++ src/main/java/com/pluralsight/ShoppingCart.java
@@ -4,53 +4,62 @@ import java.util.ArrayList;
 import javax.inject.Inject;
 
 public class ShoppingCart {
- @Inject
- private ArrayList<CartItem> cartItems = new ArrayList<CartItem>();
- private double dblOrderTotal ;
+    @Inject
+    private ArrayList<CartItem> cartItems = new ArrayList<CartItem>();
+    private double dblOrderTotal ;
 
- public int getLineItemCount() {
+    public int getLineItemCount() {
   return cartItems.size();
  }
 
- public void addCartItem(Book book, int quantity) {
-   CartItem cartItem = new CartItem(book, quantity);
-   cartItems.add(cartItem);
-   calculateOrderTotal();
- }
+    public void addCartItem(Book book, int quantity) {
+        CartItem cartItem = new CartItem(book, quantity);
+        cartItems.add(cartItem);
+        calculateOrderTotal();
+    }
 
- public void addCartItem(CartItem cartItem) {
+    public void addCartItem(CartItem cartItem) {
   cartItems.add(cartItem);
  }
 
- public CartItem getCartItem(int iItemIndex) {
-  CartItem cartItem = null;
-  if(cartItems.size()>iItemIndex) {
-   cartItem = cartItems.get(iItemIndex);
-  }
-  return cartItem;
- }
+    public void deleteCartItem(int index) {
+        try {
+            cartItems.remove(index);
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+        }
 
- public ArrayList<CartItem> getCartItems() {
-  return cartItems;
- }
- public void setCartItems(ArrayList<CartItem> cartItems) {
-  this.cartItems = cartItems;
- }
- public double getOrderTotal() {
-  return dblOrderTotal;
- }
- public void setOrderTotal(double dblOrderTotal) {
-  this.dblOrderTotal = dblOrderTotal;
- }
+    }
 
- protected void calculateOrderTotal() {
-  double dblTotal = 0;
-  for(int counter=0;counter<cartItems.size();counter++) {
-   CartItem cartItem = cartItems.get(counter);
-   dblTotal+=cartItem.getTotalCost();
+    public CartItem getCartItem(int iItemIndex) {
+        CartItem cartItem = null;
+        if(cartItems.size()>iItemIndex) {
+            cartItem = cartItems.get(iItemIndex);
+        }
+        return cartItem;
+    }
 
-  }
-  setOrderTotal(dblTotal);
- }
+    public ArrayList<CartItem> getCartItems() {
+        return cartItems;
+    }
 
+    public void setCartItems(ArrayList<CartItem> cartItems) {
+      this.cartItems = cartItems;
+    }
+
+    public double getOrderTotal() {
+      return dblOrderTotal;
+     }
+    public void setOrderTotal(double dblOrderTotal) {
+      this.dblOrderTotal = dblOrderTotal;
+     }
+
+    protected void calculateOrderTotal() {
+        double dblTotal = 0;
+        for(int counter=0;counter<cartItems.size();counter++) {
+            CartItem cartItem = cartItems.get(counter);
+            dblTotal+=cartItem.getTotalCost();
+        }
+        setOrderTotal(dblTotal);
+    }
 }
